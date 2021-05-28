04f14011f2ce97609d8dfeffc44a2a5538403ca3
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-05-28 21:15:09 +0900

    ユーザーログインの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
-  validates :first_name_kana, with: /\A[ァ-ヶー－]+\z/, presence: true
-  validates :last_name_kana, with: /\A[ァ-ヶー－]+\z/, presence: true
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
   validates :birthday, presence:true
 end
