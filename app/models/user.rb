1e24c7e271bb166ae0eb220e53757e1647e1a7ba
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-05-28 16:34:55 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  validates :email, exclusion: {:in ["@"] }, presence: true
+  validates :encrypted_password, length: { minimum: 6 }, with: /\A[a-z0-9]+\z/i, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
