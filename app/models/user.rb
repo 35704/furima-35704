c407b8d0bfd58840ebb710e5663bd66328c6f152
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-05-28 16:40:57 +0900

    本人確認バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, exclusion: {:in ["@"] }, presence: true
   validates :encrypted_password, length: { minimum: 6 }, with: /\A[a-z0-9]+\z/i, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  validates :first_name, wthh: /\A[ぁ-んァ-ヶ一-龥]/+\z, presence: true
+  validates :last_name, with: /\A[ぁ-んァ-ヶ一-龥]/+\z, presence: true
+  validates :first_name_kana, with: /\A[ァ-ヶー－]+\z/, presence: true
+  validates :last_name_kana, with: /\A[ァ-ヶー－]+\z/, presence: true
   validates :birthday, presence:true
 end
