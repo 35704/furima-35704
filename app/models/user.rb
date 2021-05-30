288517e32e807aea0108124d1f4aa680494cc6fd
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-05-30 15:52:07 +0900

    指摘修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: PASSWORD_REGEX }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end  
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do  
+      validates :first_name_kana
+      validates :last_name_kana
+    end
     validates :birthday
   end  
 end
