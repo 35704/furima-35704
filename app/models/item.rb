966acbb16ec7b4d13a9a533a268d55937d17ace6
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-05-31 11:40:38 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :category_id
+    validates :price
+    validates :condition_id
+    validates :delivery_charge_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+    validates :image
+  end
 end
