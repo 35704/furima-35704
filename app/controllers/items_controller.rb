b6f02e51cd1104eb0a1800081f7d0c919fb9cdb4
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-06-03 13:31:31 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :page_transition, only: [:edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,20 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
-    else
-      render action: :edit, except: [:edit]
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to @item
     else
@@ -44,6 +38,18 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :description, :price, :image, :category_id, :condition_id, :delivery_charge_id, :delivery_day_id, :delivery_area_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def page_transition
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    else
+      render action: :edit, except: [:edit]
+    end
   end
 
 end
