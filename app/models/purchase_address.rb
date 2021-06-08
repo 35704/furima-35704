dc3bbfb4651cf4a6cab268754a5febdd7e124820
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-06-08 18:30:48 +0900

    バリデーションの設定

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -2,5 +2,13 @@ class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :delivery_area_id, :municipalities, :address_number, :building_name, :phone_number, :user_id, :item_id
 
-  
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :user_id
+    validates :item_id
+    validates :municipalities
+    validates :address_number
+    validates :phone_number
+    validates :delivery_area_id, numericality: { other_than: 1 }
+  end
 end
