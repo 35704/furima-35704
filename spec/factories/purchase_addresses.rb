229c5d7a300d001750117402442c578a17e5a21b
35704 <try.try.buta.04190430@yahoo.ne.jp>
2021-06-10 13:52:09 +0900

    テストコード記入

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_address do
-    
+    postal_code { '123-4567' }
+    delivery_area_id { 2 }
+    municipalities { 'さいたま市' }
+    address_number { '1-1' }
+    building_name { 'さいたまハイツ' }
+    phone_number { '09012345678' }
   end
 end
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入情報の保存' do
+    before do
+      user = FactoryBot.create(:user)
+      @donation_address = FactoryBot.build(:donation_address, user_id: user.id)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+      end
+      it 'building_nameは空でも保存できること' do
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'postal_codeが空だと保存できないこと' do
+      end
+      it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
+      end
+      it 'delivery_area_idを選択していないと保存できないこと' do
+      end
+      it 'userが紐付いていないと保存できないこと' do
+      end
+    end
+  end
 end
